cc4ee2047b54e766112d4238c77f2b8fc68d485e
岡田俊樹 <>
2019-06-05 17:34:51 +0900

    psqlにデータ投入

---
+++ db/schema.rb
@@ -0,0 +1,74 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20190605080816) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "results", force: :cascade do |t|
+    t.string   "MachineGuid"
+    t.string   "Cleared"
+    t.string   "DateTime"
+    t.string   "Disqualified"
+    t.string   "Grade"
+    t.string   "Guid"
+    t.string   "Held"
+    t.string   "LetGo"
+    t.string   "MissedHold"
+    t.string   "LifeRemainingSeconds"
+    t.string   "MaxCombo"
+    t.string   "Modifiers"
+    t.string   "Pad"
+    t.string   "PercentDP"
+    t.string   "PlayerGuid"
+    t.string   "ProductID"
+    t.string   "Air"
+    t.string   "Chaos"
+    t.string   "Fakes"
+    t.string   "Freeze"
+    t.string   "Hands"
+    t.string   "Holds"
+    t.string   "Jumps"
+    t.string   "Lifts"
+    t.string   "Mines"
+    t.string   "Notes"
+    t.string   "Rolls"
+    t.string   "Stream"
+    t.string   "TapsAndHolds"
+    t.string   "Voltage"
+    t.string   "Score"
+    t.string   "StageGuid"
+    t.string   "SurviveSeconds"
+    t.string   "AvoidMine"
+    t.string   "CheckpointHit"
+    t.string   "CheckpointMiss"
+    t.string   "HitMine"
+    t.string   "Miss"
+    t.string   "W1"
+    t.string   "W2"
+    t.string   "W3"
+    t.string   "W4"
+    t.string   "W5"
+    t.string   "Dir"
+    t.string   "Difficulty"
+    t.string   "Level"
+    t.string   "StepsType"
+    t.string   "_id"
+    t.string   "_index"
+    t.string   "_score"
+    t.string   "_type"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+end
