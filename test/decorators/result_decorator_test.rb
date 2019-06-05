2d4ed00cc6098cd5367e2cf4b4977f0aa0cd80ef
konoyono <okada.toshiki623@gmail.com>
2019-06-06 07:41:36 +0900

    add decorator for showing result

---
+++ test/decorators/result_decorator_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ResultDecoratorTest < ActiveSupport::TestCase
+  def setup
+    @result = Result.new.extend ResultDecorator
+  end
+
+  # test "the truth" do
+  #   assert true
+  # end
+end
