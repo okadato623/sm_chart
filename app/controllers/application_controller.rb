f8877c341e257b9da2ee18ce4660062d9747dc24
岡田俊樹 <>
2019-06-08 19:00:21 +0900

    体裁調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :basic
+
+  private
+  def basic
+    authenticate_or_request_with_http_basic do |name, password|
+      name == "okadato" && password == "okadato"
+    end
+  end
 end
