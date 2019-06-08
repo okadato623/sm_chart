0d076a6eeb05c04f717aac8147d9d0c5614a1982
konoyono <okada.toshiki623@gmail.com>
2019-06-08 14:08:14 +0900

    x軸逆転

--- app/controllers/chart_sample_controller.rb
+++ app/controllers/chart_sample_controller.rb
@@ -1,9 +1,8 @@
 class ChartSampleController < ApplicationController
   def index
-    @results = Result.all
     gon.dates = []
     gon.allResults = []
-    Result.pluck(:DateTime, :Cleared, :Level, :Dir, :Difficulty, :Grade).each{ |result|
+    Result.pluck(:DateTime, :Cleared, :Level, :Dir, :Difficulty, :Grade).reverse.each{ |result|
       result[0] = result[0].to_date.strftime("%m月%d日")
       gon.allResults << result
       gon.dates << result[0]
