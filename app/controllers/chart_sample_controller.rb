ec63b0b156659cea9ef4c3bdf2e380824fa52c1d
konoyono <okada.toshiki623@gmail.com>
2019-06-08 11:17:48 +0900

    表作成

--- app/controllers/chart_sample_controller.rb
+++ app/controllers/chart_sample_controller.rb
@@ -3,17 +3,10 @@ class ChartSampleController < ApplicationController
     @results = Result.all
     gon.dates = []
     gon.allResults = []
-    Result.pluck(:DateTime, :Cleared, :Level).each{ |result|
+    Result.pluck(:DateTime, :Cleared, :Level, :Dir, :Difficulty, :Grade).each{ |result|
       result[0] = result[0].to_date.strftime("%m月%d日")
       gon.allResults << result
       gon.dates << result[0]
     }
-
-    gon.playCount = []
-    arDate = Result.pluck(:DateTime)
-    eachDate = arDate.each_with_object(Hash.new(0)){|v,o| o[v]+=1}
-    eachDate.each_value do |v|
-      gon.playCount << v
-    end
   end
 end
